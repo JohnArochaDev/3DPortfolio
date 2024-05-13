fdf64357856ca4a4d59ef16cd87834c0db734967
JohnDev <jparocha777@gmail.com>
2024-05-13 16:25:27 -0500

    added psudocode t oexplain next steps

--- src/Components/LoadingScreen.jsx
+++ src/Components/LoadingScreen.jsx
@@ -1,4 +1,16 @@
+import { useProgress } from '@react-three/drei'
+
 export default function LoadingScreen() {
+
+    const { active, progress, errors, item, loaded, total } = useProgress()
+
+    // active is a boolean that indicates if the loader is active
+    // progress is a number between 0 and 100
+    // errors is an array of errors that occurred
+    // item is the current item being loaded
+    // loaded is the number of items loaded
+    // total is the total number of items to be loaded, but it doesnt work, so the number as of now is 21 to be pitted against the 21 items in the scene
+
     return (
         <div className="loadingScreen">
             <h1>Loading...</h1>
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-import { Loader, useProgress } from '@react-three/drei'
 import { Suspense } from 'react'
 
 import App from './Components/App'
@@ -10,15 +9,6 @@ import LoadingScreen from './Components/LoadingScreen'
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 function Index() {
-
-    const { active, progress, errors, item, loaded, total } = useProgress()
-    console.log('active',active)
-    console.log('progress',progress)
-    console.log('errors',errors)
-    console.log('item',item)
-    console.log('loaded',loaded)
-    console.log('total',total)
-
 
 
     return (
