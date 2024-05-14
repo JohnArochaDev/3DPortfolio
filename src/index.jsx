b7f30fc2a08e1a4caa80e6beec520efbb106c7cf
JohnDev <jparocha777@gmail.com>
2024-05-14 15:59:26 -0500

    added a component for tips

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,23 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 
 import App from './Components/App'
 import LoadingScreen from './Components/LoadingScreen'
+import Startup from './Components/Startup'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 function Index() {
 
+    const [loaded, setLoaded] = useState(false)
+
 
     return (
         <>
-            <LoadingScreen />
+        {loaded ? <Startup loaded={loaded} setLoaded={setLoaded}  /> : null}
+            <LoadingScreen/>
             <Canvas gl={{ antialias: true }} >
                 <Suspense fallback={null}>
                     <App />
